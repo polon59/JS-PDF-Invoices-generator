02f2a653119546625e7f93f9d206cb2348991e2f
polon59 <przemyslaw.polczak@gmail.com>
2019-02-06 23:55:38 +0100

    Add index.ejs file in views folder

--- app.js
+++ app.js
@@ -10,8 +10,32 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+const users = [
+    {
+        id: 1,
+        firstname: 'John',
+        lastname: 'Doe',
+        email: 'johnDoe@gmail.com'
+    },
+    {
+        id: 2,
+        firstname: 'Jill',
+        lastname: 'Hub',
+        email: 'jj@gmail.com'
+    },
+    {
+        id: 3,
+        firstname: 'Jack',
+        lastname: 'Doe',
+        email: 'johnjackDoe@gmail.com'
+    }
+]
+
+
 app.get('/', (req, res) =>{
-    res.send("homepage");
+    res.render("index", {
+        users
+    });
 });
 
 
