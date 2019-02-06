b44851ec4eba0de3fca9f737c28a454bab07b140
polon59 <przemyslaw.polczak@gmail.com>
2019-02-06 23:12:59 +0100

    Add body-parser middleware in app.js

--- app.js
+++ app.js
@@ -3,8 +3,15 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 8000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
 
 
+
+
+app.get('/', (req, res) =>{
+    res.send("homepage");
+});
 
 
 app.listen(port, () => {console.log(`listening on port ${port}`)});
