fac7e7eb7e03ebc442a958a9e40c4afddb4d0dff
polon59 <przemyslaw.polczak@gmail.com>
2019-02-07 23:50:33 +0100

    Implement constructor in class 'Invoice'

--- model/Invoice.js
+++ model/Invoice.js
@@ -1,7 +1,14 @@
+const Service = require('./Service.js');
+
 class Invoice{
 
     constructor(){
-
+        this.id = id;
+        this.title = title;
+        this.date = date;
+        this.billFrom = billFrom;
+        this.billTo = billTo;
+        this.services = [];
     }
 }
 
