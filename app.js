f440301f8be947f344174769e5b1c92b71ead7ff
polon59 <przemyslaw.polczak@gmail.com>
2019-02-09 19:12:36 +0100

    Add 'accountController' 'setRoute'  method invoke in app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const InvoicesController = require('./InvoicesController.js');
+const AccountController = require('./AccountController.js');
 const app = express();
 const port = process.env.PORT || 8000;
 app.use(bodyParser.json());
@@ -12,7 +13,10 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 var invoicesController = new InvoicesController(app);
+var accountController = new AcccountController(app);
 invoicesController.setRoutes();
+accountController.setRoutes();
+
 
 
 app.get('/', (req, res) =>{
