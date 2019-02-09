a39ecdbcf2df0c44fbef36d11ab584e133b9b57b
polon59 <przemyslaw.polczak@gmail.com>
2019-02-09 19:11:36 +0100

    Implement method 'setRouteMyaccount' in class 'AccountController.js'

---
+++ AccountController.js
@@ -0,0 +1,35 @@
+const Invoice = require('./model/Invoice.js');
+
+class AccountController{
+    
+        constructor(app){
+            this.app = app;
+            this.currentUser = undefined;
+        }
+
+
+        addNewUser(){
+            
+        }
+
+
+        setRoutes(){
+            this.setRouteMyAccount();
+        }
+
+
+        setRouteMyAccount(){
+            this.app.post('/myAccount', (req, res) =>{
+                const login = req.body.login;
+            
+                console.log(`[INFO] logged user: (password:${req.body.password} , login: ${login})`);
+            
+                res.render("myAccount", {
+                    login
+                });
+            });
+        }
+
+    }
+    
+    module.exports = AccountController;
