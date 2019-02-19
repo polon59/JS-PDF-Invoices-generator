4cef256d2271e7ea86ef0858965ba735bffc0414
polon59 <przemyslaw.polczak@gmail.com>
2019-02-19 22:53:57 +0100

    Add if statement in 'invoicesList' component to inform if there are no active invoices available

--- public/application/src/InvoicesList.js
+++ public/application/src/InvoicesList.js
@@ -2,35 +2,44 @@ import React from 'react';
 
 const InvoicesList = (props)=>{
     const { invoices } = props;
-    const { deleteInvoice } = props;
-    const invoicesList = invoices.map(invoice =>{
-        return (
-            <tr key={invoice.id}>
-                <td>{invoice.id}</td>
-                <td>{invoice.title}</td>
-                <td>{invoice.billTo}</td>
-                <td><button onClick={()=>{deleteInvoice(invoice.id)}}>X</button></td>
-            </tr>
+    if (invoices.length > 0){
+        const { deleteInvoice } = props;
+        const invoicesList = invoices.map(invoice =>{
+            return (
+                <tr key={invoice.id}>
+                    <td>{invoice.id}</td>
+                    <td>{invoice.title}</td>
+                    <td>{invoice.billTo}</td>
+                    <td><button onClick={()=>{deleteInvoice(invoice.id)}}>X</button></td>
+                </tr>
+            );
+        });
+        return(
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>TITLE</th>
+                            <th>BILL TO</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {invoicesList}
+                    </tbody>
+                </table>
+                
+            </div>
         );
-    });
-
-    return(
-        <div>
-            <table>
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>TITLE</th>
-                        <th>BILL TO</th>
-                    </tr>
-                </thead>
-                <tbody>
-                {invoicesList}
-                </tbody>
-            </table>
-            
-        </div>
-    );
+    }
+    else{
+        return (
+            <div>
+                <h3>You have no active invoices</h3>
+            </div>
+        )
+    }
+    
 }
 
 export default InvoicesList;
