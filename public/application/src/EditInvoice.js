9192b5ffe86e697a6ebc626095070fbb73883eae
polon59 <przemyslaw.polczak@gmail.com>
2019-02-20 22:04:27 +0100

    Implement function 'handleChange' in 'EditInvoice' component

--- public/application/src/EditInvoice.js
+++ public/application/src/EditInvoice.js
@@ -2,27 +2,44 @@ import React, { Component } from 'react';
 
 class EditInvoice extends Component{
     state = {
-        title: null,
-        billTo: null
+        title: this.props.invoiceToEdit.title,
+        billTo: this.props.invoiceToEdit.billTo
     }
 
     handleSubmit = (e) =>{
         e.preventDefault();
-        this.props.addInvoice(this.state);
+        this.props.updateInvoice(this.state);
     }
 
+    handleChange = (e) => {
+        this.setState({
+            [e.target.id] : e.target.value
+        });
+    }
+
+
     render(){
-        return(
-            <div>
-                <form onSubmit={this.handleSubmit}>
-                    <label htmlFor="title">Title</label>
-                    <input type="text" id="title" onChange={this.handleChange}/>
-                    <label htmlFor="title">BillTo</label>
-                    <input type="text" id="billTo" onChange={this.handleChange}/>
-                    <button>Submit</button>
-                </form>
-            </div>
-        )
+        if(this.props.invoiceToEdit === ""){
+            return(
+                <div className="bordered">
+                    <h3>No invoice to edit chosen</h3>
+                </div>
+            )
+        }
+        else{
+            return(
+                <div className="bordered">
+                    <h3>EDIT INVOICE</h3>
+                    <form onSubmit={this.handleSubmit}>
+                        <label htmlFor="title">Title</label>
+                        <input type="text" id="title" value={this.state.title} onChange={this.handleChange}/>
+                        <label htmlFor="title">BillTo</label>
+                        <input type="text" id="billTo" value={this.state.billTo} onChange={this.handleChange}/>
+                        <button>Submit</button>
+                    </form>
+                </div>
+            )
+        }
     }
 }
 
