c2cc75bf7ba1905819836dda14258d2f2e2369f8
polon59 <przemyslaw.polczak@gmail.com>
2019-02-21 12:10:37 +0100

    Remove unused code lines from 'render' method in 'Services' component

--- public/application/src/Services.js
+++ public/application/src/Services.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-class EditInvoice extends Component{
+class Services extends Component{
 
 
     handleChange = (e) => {
@@ -46,34 +46,6 @@ class EditInvoice extends Component{
                 </div>
             )
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        <form onSubmit={this.handleSubmit}>
-            <label>Description</label>
-            <input type="text" id="description" value={this.props.invoiceToEdit.title} onChange={this.handleChange}/>
-            <label>Bill to:</label>
-            <input type="text" id="billTo" value={this.props.invoiceToEdit.billTo} onChange={this.handleChange}/>
-            <label>Bill from:</label>
-            <input type="text" id="billFrom" value={this.props.invoiceToEdit.billFrom} onChange={this.handleChange}/>
-            <label>Services:</label>
-            <Services services={this.props.invoiceToEdit.services}/>
-            {/* <input type="text" id="services" value={this.props.invoiceToEdit.services} onChange={this.handleChange}/> */}
-            <button>Submit</button>
-        </form>
     }
 }
 
