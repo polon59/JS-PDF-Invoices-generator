3468e5ac4b635ec381fc48bf7f8ca6d1053ea894
polon59 <przemyslaw.polczak@gmail.com>
2019-02-23 17:38:44 +0100

    Add 'EditInvoice' component to returned content in 'render' method in ''AddInvoice' class

--- public/application/src/AddInvoice.js
+++ public/application/src/AddInvoice.js
@@ -1,28 +1,21 @@
 import React, { Component } from 'react';
+import EditInvoice from './EditInvoice';
 
 class AddInvoice extends Component{
 
-
-    handleSubmit = (e) =>{
-        e.preventDefault();
-        this.setState({
-            id : Math.random()
-        })
-        this.props.addInvoice(this.state);
+    handleSubmit = () =>{
+        this.props.addCreatedInvoiceToList();
     }
 
     render(){
+        
+        const {invoiceToEdit,deleteService,addService,changeInvoiceToEdit} = this.props;
         return(
-            <div className="bordered">
-                <h3>ADD NEW INVOICE</h3>
-                <form onSubmit={this.handleSubmit}>
-                    <label htmlFor="title">Title</label>
-                    <input type="text" id="title" onChange={this.handleChange}/>
-                    <label htmlFor="title">BillTo</label>
-                    <input type="text" id="billTo" onChange={this.handleChange}/>
-                    <button>Submit</button>
-                </form>
-            </div>
+            <EditInvoice deleteServiceFromInvoice={deleteService} 
+            saveChanges={this.handleSubmit} 
+            addService={addService} 
+            changeInvoiceToEdit={changeInvoiceToEdit} 
+            invoiceToEdit={invoiceToEdit}/>            
         )
     }
 }
