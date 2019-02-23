10c30bac8bc5c36e068c1f278dd2c7d698adfe64
polon59 <przemyslaw.polczak@gmail.com>
2019-02-23 15:38:59 +0100

    Implement 'render' method in 'MyAccount' component

--- public/application/src/MyAccount.js
+++ public/application/src/MyAccount.js
@@ -4,7 +4,11 @@ class MyAccount extends Component{
 
     render(){
         return(
-            <div></div>
+            <div>
+                <div className="optionBox bordered"><h4>View current invoices</h4></div>
+                <div className="optionBox bordered"><h4>Create new invoice</h4></div>
+                <div className="optionBox bordered"><h4>View statistics</h4></div>
+            </div>
         );
     }
 }
