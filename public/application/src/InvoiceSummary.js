e2a89f3b138aa7b9f71e3d80bd5e2ecdeaad2cad
polon59 <przemyslaw.polczak@gmail.com>
2019-02-25 16:44:57 +0100

    Implement return JSX value in 'InvoiceSummary' component

--- public/application/src/InvoiceSummary.js
+++ public/application/src/InvoiceSummary.js
@@ -1,7 +1,29 @@
 import React from 'react';
 
 const InvoiceSummary = (props) =>{
-
+    const {subTotal,salesTax,salesTaxVal,totalDue} = props.invoiceToEdit;
+    return (
+        <table>
+            <tbody>
+                <tr>
+                    <td>SUBTOTAL:</td>
+                    <td>{subTotal}</td>
+                </tr>
+                <tr>
+                    <td>SALES TAX %:</td>
+                    <td><input type="number" min="0" max="100" id="salesTax" value={salesTax} onChange={props.handleChange}/></td>
+                </tr>
+                <tr>
+                    <td>SALES TAX:</td>
+                    <td>{salesTaxVal}</td>
+                </tr>
+                <tr>
+                    <td><h4>TOTAL DUE:</h4></td>
+                    <td><h4>{totalDue}</h4></td>
+                </tr>
+            </tbody>
+        </table>
+    );
 }
 
 export default InvoiceSummary;
