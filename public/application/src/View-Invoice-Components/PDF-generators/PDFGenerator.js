6a0ccdb4b2bc44bb0957caaa4aa8aef73299ae95
polon59 <przemyslaw.polczak@gmail.com>
2019-02-26 21:55:32 +0100

    Add canvas modules to pass screenshot of invoice to PDFGenerator

--- public/application/src/View-Invoice-Components/PDF-generators/PDFGenerator.js
+++ public/application/src/View-Invoice-Components/PDF-generators/PDFGenerator.js
@@ -1,25 +1,39 @@
 import * as JsPDF from 'jspdf'
+import * as html2canvas from 'html2canvas';
 
 class PDFGenerator{
 
-    constructor(){
-        this.doc = new JsPDF();
-    }
-
     generateDocument = () =>{
-        const invoice = document.getElementById("PDF-Content").innerHTML;
+        const invoice = document.getElementById("PDF-Content");
 
-        let specialElementHandlers = {
-            '#editor': function (element, renderer) {
-                return true;
-            }
-        };
-
-        this.doc.fromHTML(invoice, 10, 10, {
-            'width': 170,
-            'elementHandlers': specialElementHandlers
+        html2canvas(invoice).then(canvas => {
+            const img = canvas.toDataURL("image/png");
+            let doc = new JsPDF();
+            doc.addImage(img, 'JPEG',20,20);
+            doc.save('document.pdf');
         });
-        this.doc.save('document.pdf');
+
+
+
+        // html2canvas(invoice, {
+        //     onrendered:function (canvas){
+        //         const img = canvas.toDataURL("image/png");
+        //         let doc = new JsPDF();
+        //         doc.addImage(img, 'JPEG',20,20);
+        //         doc.save('document.pdf');
+        //     }});
+
+        // let specialElementHandlers = {
+        //     '#editor': function (element, renderer) {
+        //         return true;
+        //     }
+        // };
+
+        // this.doc.fromHTML(invoice, 10, 10, {
+        //     'width': 170,
+        //     'elementHandlers': specialElementHandlers
+        // });
+        // this.doc.save('document.pdf');
     }
 }
 
