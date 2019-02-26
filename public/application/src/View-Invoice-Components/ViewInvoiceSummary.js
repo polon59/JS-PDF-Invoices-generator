e9c240bbd767965e436e12e6052f95780b7cb9b4
polon59 <przemyslaw.polczak@gmail.com>
2019-02-26 17:51:48 +0100

    Implement render in 'ViewInvoiceSummary.js'

--- public/application/src/View-Invoice-Components/ViewInvoiceSummary.js
+++ public/application/src/View-Invoice-Components/ViewInvoiceSummary.js
@@ -1,7 +1,29 @@
 import React from 'react'
 
 const ViewInvoiceSummary = (props) =>{
-
+    const {subTotal,salesTax,salesTaxVal,totalDue} = props.invoiceToEdit;
+    return (
+        <table>
+            <tbody>
+                <tr>
+                    <td>SUBTOTAL:</td>
+                    <td>{subTotal.toFixed(2)}</td>
+                </tr>
+                <tr>
+                    <td>SALES TAX %:</td>
+                    <td>{salesTax}</td>
+                </tr>
+                <tr>
+                    <td>SALES TAX:</td>
+                    <td>{salesTaxVal.toFixed(2)}</td>
+                </tr>
+                <tr>
+                    <td><h4>TOTAL DUE:</h4></td>
+                    <td><h4>{totalDue.toFixed(2)}</h4></td>
+                </tr>
+            </tbody>
+        </table>
+    );
 }
 
 export default ViewInvoiceSummary;
