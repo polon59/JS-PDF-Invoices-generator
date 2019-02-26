2766832df1310ccc6c503e095b65ea50a27b6830
polon59 <przemyslaw.polczak@gmail.com>
2019-02-26 18:16:48 +0100

    Add new button for displaying invoice

--- public/application/src/InvoicesList.js
+++ public/application/src/InvoicesList.js
@@ -3,15 +3,16 @@ import React from 'react';
 const InvoicesList = (props)=>{
     const { invoices } = props;
     if (invoices.length > 0){
-        const { deleteInvoice } = props;
+        const { deleteInvoice,viewInvoice } = props;
         const invoicesList = invoices.map(invoice =>{
             return (
                 <tr key={invoice.id}>
                     <td>{invoice.id}</td>
                     <td>{invoice.title}</td>
                     <td>{invoice.billTo}</td>
-                    <td><button onClick={()=>{deleteInvoice(invoice.id)}}>X</button></td>
                     <td><button onClick={()=>{props.setInvoiceToEdit(invoice.id)}}>EDIT</button></td>
+                    <td><button onClick={()=>{viewInvoice(invoice.id)}}>VIEW</button></td>
+                    <td><button onClick={()=>{deleteInvoice(invoice.id)}}>X</button></td>
                 </tr>
             );
         });
