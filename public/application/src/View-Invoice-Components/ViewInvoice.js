7b94cc25e10837bcacbb9ff4c16b34e3304e2c7b
polon59 <przemyslaw.polczak@gmail.com>
2019-02-26 17:31:12 +0100

    Add new JSX content in 'render' method

--- public/application/src/View-Invoice-Components/ViewInvoice.js
+++ public/application/src/View-Invoice-Components/ViewInvoice.js
@@ -8,15 +8,15 @@ class ViewInvoice extends Component{
         <h3>EDIT INVOICE</h3>
             <div className="invoice-header invoice-section">
                 <h4>{this.props.invoiceToEdit.title}</h4>
-                <input id="date" type="date" className="right" value={this.props.invoiceToEdit.date} onChange={this.handleChange} min="2015-01-01" required/>
+                <h4>{this.props.invoiceToEdit.date}</h4>
             </div>
             <div className="invoice-company invoice-section">
-                <textarea id="billTo" placeholder="BILL TO" value={this.props.invoiceToEdit.billTo} onChange={(e)=>{this.handleChange(e); this.resize(e.target)}} required/>
-                <textarea id="billFrom" placeholder="BILL FROM" className="right" value={this.props.invoiceToEdit.billFrom} onChange={(e)=>{this.handleChange(e); this.resize(e.target)}} required/>
-            </div>  
-            <Services handleServiceDelete={this.handleServiceDelete} calculateSubTotal={this.props.calculateSubTotal} handleChange={this.handleChange} addService={this.props.addService} services={this.props.invoiceToEdit.services}/>
-            <InvoiceSummary invoiceToEdit={this.props.invoiceToEdit} handleChange={this.handleChange}/>               
-            <input type="submit" value="Save invoice"/>
+                <h4>{this.props.invoiceToEdit.billTo}</h4>
+                <h4>{this.props.invoiceToEdit.billFrom}</h4>
+            </div>
+            <ViewServices services={this.props.invoiceToEdit.services}/>
+            <ViewInvoiceSummary invoiceToEdit={this.props.invoiceToEdit}/>
+            
             <div id="editor"></div>
             <button id="cmd" onClick={this.generatePDFFromInvoice}>generate PDF</button>
         </div>
