0f97face0be3db6a0a911d6c820d82248e8b67bf
polon59 <przemyslaw.polczak@gmail.com>
2019-02-28 15:01:32 +0100

    Remove unused variable from 'render' in 'MyAccount' component

--- public/application/src/My-Account-Components/MyAccount.js
+++ public/application/src/My-Account-Components/MyAccount.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 
 class MyAccount extends Component{
 
     render(){
-        const {changeTogle} = this.props;
         return(
             <div>
                 <div className="bordered">
                     <h3>Welcome user1</h3>
                     <p>this is some text added to look like it is nessesary, but honestly i added it to fill this empty space</p>
                 </div>
-                <div onClick={()=>{changeTogle("invoicesList")}} className="optionBox bordered"><h4>View current invoices</h4></div>
-                <div onClick={()=>{changeTogle("addInvoice")}} className="optionBox bordered"><h4>Create new invoice</h4></div>
-                <div onClick={()=>{changeTogle("statistics")}} className="optionBox bordered"><h4>View statistics</h4></div>
+                <div className="optionBox bordered"><h4>View current invoices</h4></div>
+                <div className="optionBox bordered"><h4>Create new invoice</h4></div>
+                <div className="optionBox bordered"><h4>View statistics</h4></div>
             </div>
         );
     }
