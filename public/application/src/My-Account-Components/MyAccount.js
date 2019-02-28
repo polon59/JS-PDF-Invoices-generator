1bbe2b7e0d09087606110d7e9c4d1ff776edb3fb
polon59 <przemyslaw.polczak@gmail.com>
2019-02-28 15:17:18 +0100

    Add <Link> components in 'MyAccount.js' render function

--- public/application/src/My-Account-Components/MyAccount.js
+++ public/application/src/My-Account-Components/MyAccount.js
@@ -10,9 +10,15 @@ class MyAccount extends Component{
                     <h3>Welcome user1</h3>
                     <p>this is some text added to look like it is nessesary, but honestly i added it to fill this empty space</p>
                 </div>
-                <div className="optionBox bordered"><h4>View current invoices</h4></div>
-                <div className="optionBox bordered"><h4>Create new invoice</h4></div>
-                <div className="optionBox bordered"><h4>View statistics</h4></div>
+                <Link to='/myInvoices'>
+                    <div className="optionBox bordered"><h4>View current invoices</h4></div>
+                </Link>
+                <Link to='/addInvoice'>
+                    <div className="optionBox bordered"><h4>Create new invoice</h4></div>
+                </Link>
+                <Link to='/statistics'>
+                    <div className="optionBox bordered"><h4>View statistics</h4></div>
+                </Link>
             </div>
         );
     }
