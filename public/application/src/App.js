d248e2b998bfd7a2dd0c107bf7a7a351b2dff035
polon59 <przemyslaw.polczak@gmail.com>
2019-02-28 15:25:25 +0100

    Remove unused return statement in 'createNewInvoice' function in 'App.js'

--- public/application/src/App.js
+++ public/application/src/App.js
@@ -31,7 +31,6 @@ class App extends Component {
     const id = Math.random();
     let newInvoice = {id: id, title: "", date:"2017-02-14", billTo:"", billFrom:"",subTotal:0, salesTax:0, salesTaxVal:0, totalDue:0, services:[]};
     this.saveEditedInvoice(newInvoice)
-    return newInvoice;
   }
 
 
