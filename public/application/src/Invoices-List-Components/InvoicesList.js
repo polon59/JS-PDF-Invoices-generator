490fda97d4152503c6e8adaaf569c0daff717908
polon59 <przemyslaw.polczak@gmail.com>
2019-03-05 14:53:54 +0100

    Change 'InvoicesList' component to class component instead of function

--- public/application/src/Invoices-List-Components/InvoicesList.js
+++ public/application/src/Invoices-List-Components/InvoicesList.js
@@ -1,58 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 
-const InvoicesList = (props)=>{
-    const { invoices } = props;
-    if (invoices.length > 0){
-        const { deleteInvoice,changeCurrentInvoice } = props;
-        const invoicesList = invoices.map(invoice =>{
-            return (
-                <tr key={invoice.id}>
-                    <td>{invoice.id}</td>
-                    <td>{invoice.title}</td>
-                    <td>{invoice.billTo}</td>
-                    <td>
-                        <Link to={`/myInvoices/editInvoice/${invoice.id}`}>
-                            <button onClick={()=>{props.changeCurrentInvoice(invoice.id)}}>EDIT</button>
-                        </Link>
-                    </td>
-                    <td>
-                        <Link to={`/myInvoices/viewInvoice/${invoice.id}`}>
-                            <button onClick={()=>{changeCurrentInvoice(invoice.id)}}>VIEW</button>
-                        </Link>
-                    </td>
-                    <td>
-                        <button onClick={()=>{deleteInvoice(invoice.id)}}>X</button>
-                    </td>
-                </tr>
+class InvoicesList extends Component{
+
+    render(){
+        const { invoices } = this.props;
+        if (invoices.length > 0){
+            const { deleteInvoice,changeCurrentInvoice } = this.props;
+            const invoicesList = invoices.map(invoice =>{
+                return (
+                    <tr key={invoice.id}>
+                        <td>{invoice.id}</td>
+                        <td>{invoice.title}</td>
+                        <td>{invoice.billTo}</td>
+                        <td>
+                            <Link to={`/myInvoices/editInvoice/${invoice.id}`}>
+                                <button onClick={()=>{changeCurrentInvoice(invoice.id)}}>EDIT</button>
+                            </Link>
+                        </td>
+                        <td>
+                            <Link to={`/myInvoices/viewInvoice/${invoice.id}`}>
+                                <button onClick={()=>{changeCurrentInvoice(invoice.id)}}>VIEW</button>
+                            </Link>
+                        </td>
+                        <td>
+                            <button onClick={()=>{deleteInvoice(invoice.id)}}>X</button>
+                        </td>
+                    </tr>
+                );
+            });
+            return(
+                <div className="bordered">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>TITLE</th>
+                                <th>BILL TO</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        {invoicesList}
+                        </tbody>
+                    </table>
+                    
+                </div>
             );
-        });
-        return(
-            <div className="bordered">
-                <table>
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>TITLE</th>
-                            <th>BILL TO</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    {invoicesList}
-                    </tbody>
-                </table>
-                
-            </div>
-        );
-    }
-    else{
-        return (
-            <div className="bordered">
-                <h3>You have no active invoices</h3>
-            </div>
-        )
-    }
-    
+        }
+        else{
+            return (
+                <div className="bordered">
+                    <h3>You have no active invoices</h3>
+                </div>
+            )
+        }
+    } 
 }
 
 export default InvoicesList;
