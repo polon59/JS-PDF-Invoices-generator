1b934caee18828d484c0e42d0cc4ed4bfe2667bc
polon59 <przemyslaw.polczak@gmail.com>
2019-03-08 00:05:53 +0100

    Implement method 'renderInvoicesList' in 'InvoicesListComponent'

--- public/application/src/Invoices-List-Components/InvoicesList.js
+++ public/application/src/Invoices-List-Components/InvoicesList.js
@@ -3,32 +3,38 @@ import {Link} from 'react-router-dom';
 
 class InvoicesList extends Component{
 
+    renderInvoicesList(){
+        const { invoices } = this.props;
+        const { deleteInvoice,changeCurrentInvoice } = this.props;
+        const invoicesList = invoices.map(invoice =>{
+            return (
+                <tr key={invoice.id}>
+                    <td>{invoice.id}</td>
+                    <td>{invoice.title}</td>
+                    <td>{invoice.billTo}</td>
+                    <td>
+                        <Link to={`/myInvoices/editInvoice/${invoice.id}`}>
+                            <button onClick={()=>{changeCurrentInvoice(invoice.id)}}>EDIT</button>
+                        </Link>
+                    </td>
+                    <td>
+                        <Link to={`/myInvoices/viewInvoice/${invoice.id}`}>
+                            <button onClick={()=>{changeCurrentInvoice(invoice.id)}}>VIEW</button>
+                        </Link>
+                    </td>
+                    <td>
+                        <button onClick={()=>{deleteInvoice(invoice.id)}}>X</button>
+                    </td>
+                </tr>
+            );
+        });
+        return invoicesList;
+    }
+
     render(){
         const { invoices } = this.props;
+        const invoicesList = this.renderInvoicesList();
         if (invoices.length > 0){
-            const { deleteInvoice,changeCurrentInvoice } = this.props;
-            const invoicesList = invoices.map(invoice =>{
-                return (
-                    <tr key={invoice.id}>
-                        <td>{invoice.id}</td>
-                        <td>{invoice.title}</td>
-                        <td>{invoice.billTo}</td>
-                        <td>
-                            <Link to={`/myInvoices/editInvoice/${invoice.id}`}>
-                                <button onClick={()=>{changeCurrentInvoice(invoice.id)}}>EDIT</button>
-                            </Link>
-                        </td>
-                        <td>
-                            <Link to={`/myInvoices/viewInvoice/${invoice.id}`}>
-                                <button onClick={()=>{changeCurrentInvoice(invoice.id)}}>VIEW</button>
-                            </Link>
-                        </td>
-                        <td>
-                            <button onClick={()=>{deleteInvoice(invoice.id)}}>X</button>
-                        </td>
-                    </tr>
-                );
-            });
             return(
                 <div className="bordered">
                     <table>
