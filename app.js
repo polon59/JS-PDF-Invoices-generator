41943c4389c35777043e48ae987f40c3964d0944
polon59 <przemyslaw.polczak@gmail.com>
2019-03-12 18:16:55 +0100

    Remove connemted code lines from 'app.js'

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ const app = express();
 const port = process.env.PORT || 8000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
-// set static path
-// app.use(express.static(path.join(__dirname, 'public')));
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
