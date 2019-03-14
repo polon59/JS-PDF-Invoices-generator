32c5aec1c97e92c9329546354e2dbccc2e00c495
polon59 <przemyslaw.polczak@gmail.com>
2019-03-14 13:09:46 +0100

    Implement class 'service.js' in 'model' folder

--- public/application/src/model/service.js
+++ public/application/src/model/service.js
@@ -1,6 +1,11 @@
 class Service{
-    
+   
     constructor(){
-
+        this.id = Math.random();
+        this.description = "";
+        this.quantity = "";
+        this.unitPrice = "";
     }
 }
+
+export default Service;
