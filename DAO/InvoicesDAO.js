1db3704fc77954d1f52c4efeddd884009bab2deb
polon59 <przemyslaw.polczak@gmail.com>
2019-03-14 18:24:04 +0100

    Implement function 'getAllInvoices' in 'InvoicesDAO.js'

--- DAO/InvoicesDAO.js
+++ DAO/InvoicesDAO.js
@@ -1,3 +1,6 @@
+const DbCon = require('./dbConn');
+const mysql=require('mysql');
+
 class InvoicesDAO{
     constructor(){
         this.invoices = [
@@ -7,10 +10,32 @@ class InvoicesDAO{
             {id: 4, title: "invoice 9923/22", date:"2017-12-23", billTo:"Radosław Turkuć-Podjadek", billFrom:"My company 4", subTotal:5001.45, salesTax:0, salesTaxVal:0, totalDue:0, services:[{id:6, description:"descr", quantity:5, unitPrice:80}, {id:7, description:"descr", quantity:2, unitPrice:1705}, {id:8, description:"descr", quantity:7, unitPrice:1200}]},
             {id: 5, title: "invoice 223/5", date:"2017-10-09", billTo:"Joanna Drapieżna", billFrom:"My company 5", subTotal:0, salesTax:120, salesTaxVal:0, totalDue:0, services:[{id:9, description:"descr", quantity:10, unitPrice:1200}, {id:10, description:"descr", quantity:1, unitPrice:11200},{id:11, description:"descr", quantity:10, unitPrice:1200}, {id:12, description:"descr", quantity:5, unitPrice:10}]}
           ];
+          this.dataBaseConn = new DbCon();
+          this.connection = this.dataBaseConn.connection;
     }
 
     getAllInvoices(){
-        return this.invoices;
+        return new Promise((resolve, reject)=>{
+            this.connection.query(
+                "SELECT * FROM invoices", 
+                function(err, result){                                                
+                    if(result === undefined){
+                        reject(new Error("Error result is undefined"));
+                    }else{
+                        resolve(result);
+                    }
+                }
+            )}
+        );
+
+
+
+        // return this.connection.query("SELECT * FROM invoices").then(
+        // function (err, result, fields) {
+        //     if (err) throw err;
+        //     return result;
+        //     console.log(result[0].date.toString());
+        //   });
     }
 
     addNewInvoice(newInvoice){
