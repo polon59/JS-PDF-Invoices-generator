4b91699b4614b7afc17cb7d46b000255d348a0ff
polon59 <przemyslaw.polczak@gmail.com>
2019-03-15 06:25:11 +0100

    Implement function 'getLastAssignedID'

--- public/application/src/DBAcces/DBAcces.js
+++ public/application/src/DBAcces/DBAcces.js
@@ -15,15 +15,29 @@ class DBAcces{
 
     addInvoiceToDB = (invoiceToAdd) =>{
         fetch('http://localhost:8000/myAccount/invoices', {
-           method: "POST",
+           method: "PUT",
            headers: {
              'Accept': 'application/json',
              'Content-Type': 'application/json',
            },
            body: JSON.stringify(invoiceToAdd)
          })
-         .then(function(response){   
-         });
+        //  .then(function(response){
+        //    console.log("assigned id:") 
+        //  });
+    }
+
+    getLastAssignedID = () =>{
+      return fetch('http://localhost:8000/myAccount/invoices/addInvoice')
+        .then(response => response.json())
+        .then(data=>{
+          console.log(data);
+          return data['LAST_INSERT_ID()'];
+        })
+        .catch(error => {
+          // alert("Warning: You are in offline mode, Your invoices cannot be loaded");
+          return "DUPA";
+          });
     }
 
     updateInvoice = (updatedInvoice) =>{
