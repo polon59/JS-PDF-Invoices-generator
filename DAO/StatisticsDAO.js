717ab01cd900cd71c88292b4ca8361dbf2531e92
polon59 <przemyslaw.polczak@gmail.com>
2019-03-18 23:33:41 +0100

    Implement method 'getNumberOfInvoicesByMonthInYear' in 'StatisticsDAO.js'

--- DAO/StatisticsDAO.js
+++ DAO/StatisticsDAO.js
@@ -3,6 +3,22 @@ class StatisticsDAO{
     constructor(connection){
         this.connection = connection;
     }
+
+
+    getNumberOfInvoicesByMonthInYear(year){
+        const sql = 
+        `SELECT MONTH(date) as month, COUNT(*) as createdInvoices
+        FROM invoices
+        WHERE YEAR(date) = ${year}
+        GROUP BY month;`;
+        connection.query(sql, function (err, result, fields) {
+          if (err) throw err;
+          console.log(result);
+        //   returned object:
+        //   [ RowDataPacket { month: 'June', createdInvoices: 1 },
+        //   RowDataPacket { month: 'March', createdInvoices: 2 } ]
+        });
+    }
 }
 
 module.exports = StatisticsDAO;
