e11dc6c32028da259f7b22198cf7edcbbce2c835
polon59 <przemyslaw.polczak@gmail.com>
2019-03-18 23:51:04 +0100

    Implement method 'getNumberOfServicesByMonthInYear' in 'StatisticsDAO.js'

--- DAO/StatisticsDAO.js
+++ DAO/StatisticsDAO.js
@@ -4,11 +4,26 @@ class StatisticsDAO{
         this.connection = connection;
     }
 
+    getNumberOfServicesByMonthInYear(year){
+        const sql = 
+        `SELECT  MONTH(date) as month, COUNT(*) as doneServices FROM invoices 
+        JOIN services ON invoices.id = services.invoiceID
+        WHERE YEAR(date) = ${year}
+        GROUP BY month;`;
+        connection.query(sql, function (err, result, fields) {
+          if (err) throw err;
+          console.log(result);
+        //   returned object:
+        // [ RowDataPacket { month: 3, doneServices: 7 },
+        //     RowDataPacket { month: 6, doneServices: 1 },
+        //     RowDataPacket { month: 12, doneServices: 4 } ]
+          
+        });
+    }
 
     getNumberOfInvoicesByMonthInYear(year){
         const sql = 
-        `SELECT MONTH(date) as month, COUNT(*) as createdInvoices
-        FROM invoices
+        `SELECT MONTH(date) as month, COUNT(*) as createdInvoices FROM invoices
         WHERE YEAR(date) = ${year}
         GROUP BY month;`;
         connection.query(sql, function (err, result, fields) {
