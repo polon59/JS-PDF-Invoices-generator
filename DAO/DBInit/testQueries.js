a288fb735acd10912bebca0a7854a325c4d217d4
polon59 <przemyslaw.polczak@gmail.com>
2019-03-19 00:38:57 +0100

    Test select query in 'testQueries.js'

--- DAO/DBInit/testQueries.js
+++ DAO/DBInit/testQueries.js
@@ -3,11 +3,18 @@ var dataBaseConn = new DbConn();
 var connection = dataBaseConn.connection;
 
 runQuery();
-
+// SELECT MONTH(date) as month, COUNT(*) as doneServices
 function runQuery() {
-    const sql = `SELECT * FROM services`;
+    const year = 2019;
+    const sql =`SELECT  MONTH(date) as month, ROUND(SUM(subTotal), 2) as income
+        FROM invoices
+        WHERE YEAR(date) = ${year}
+        GROUP BY month;`;
+
+
     connection.query(sql, function (err, result, fields) {
       if (err) throw err;
       console.log(result);
+      process.exit();
     });
   }
