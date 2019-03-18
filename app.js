85bbcd6884d485abd165930b2993edcbbece0eba
polon59 <przemyslaw.polczak@gmail.com>
2019-03-18 21:58:01 +0100

    Add creating dadabase connection in 'app.js' and passing it to controllers

--- app.js
+++ app.js
@@ -2,13 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const InvoicesController = require('./InvoicesController.js');
 const AccountController = require('./AccountController.js');
+const StatisticsController = require('./StatisticsController');
+const DbCon = require('./DAO/dbConn');
 const app = express();
 const port = process.env.PORT || 8000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
-var invoicesController = new InvoicesController(app);
+const dataBaseConn = new DbCon();
+const connection = dataBaseConn.connection;
+
+var invoicesController = new InvoicesController(app,connection);
 var accountController = new AccountController(app);
+var statisticsController = new StatisticsController(app,connection);
 invoicesController.setRoutes();
 accountController.setRoutes();
 
