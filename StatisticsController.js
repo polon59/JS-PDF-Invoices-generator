9e49b2b1cbe5ca73e175d45458b3a3cd83818f6b
polon59 <przemyslaw.polczak@gmail.com>
2019-03-19 18:54:19 +0100

    Implment method 'setRouteGetStatisticsForGivenYear' in 'StatisticsController.js'

--- StatisticsController.js
+++ StatisticsController.js
@@ -6,6 +6,21 @@ class StatisticsController{
         this.app = app;
         this.statisticsDAO = new StatisticsDAO(connection);
     }
+
+    setRoutes(){
+        this.setRouteGetStatisticsForGivenYear();
+    }
+
+    setRouteGetStatisticsForGivenYear(){
+        this.app.get('/myAccount/statistics/:year', (req,res) =>{
+            const year = req.params.year;
+            this.statisticsDAO.getAllStatistics(year)
+            .then((result)=>{
+                res.send(result);
+            })
+            .catch(error => {res.send("ERROR");console.log(error.message)});
+        });
+    }
 }
 
 module.exports = StatisticsController;
