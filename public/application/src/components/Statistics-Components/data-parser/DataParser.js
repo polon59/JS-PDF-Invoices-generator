9146a5287f7a24e7069b7bd9b93e3c51109aaa35
polon59 <przemyslaw.polczak@gmail.com>
2019-03-20 05:42:33 +0100

    Set 'month' property for statistics data set to months names instead of numbers

--- public/application/src/components/Statistics-Components/data-parser/DataParser.js
+++ public/application/src/components/Statistics-Components/data-parser/DataParser.js
@@ -2,22 +2,23 @@ class StatisticsDataParser{
     // converts JSON recieved from fetch to array of objects readable for AreaChart component
 
     parseDataForStatistics = (data)=>{
+        const monthNames = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sept','Oct','Nov','Dec'];
         let parsedData = [];
         data.forEach((dataElement) => {
             let currentStatsName = Object.keys(dataElement)[0];
-            let formattedDataElement =  {} ;
+            let formattedDataElement =  [] ;
             formattedDataElement[currentStatsName] = [];
             for (let monthNo = 1; monthNo < 13; monthNo++) {
                 let dataElements = dataElement[Object.keys(dataElement)[0]]
                 let formattedRecord = {};
-                formattedRecord['month'] = monthNo;
+                formattedRecord['month'] = monthNames[monthNo-1];
                 formattedRecord[currentStatsName] = 0;
                 dataElements.forEach(record => {
                     if (record.month === monthNo) {
                         formattedRecord[currentStatsName] = record[Object.keys(record)[1]];
                     }
                 });
-                formattedDataElement[currentStatsName].push(formattedRecord);
+                formattedDataElement.push(formattedRecord);
                 
             }
             parsedData.push(formattedDataElement);
