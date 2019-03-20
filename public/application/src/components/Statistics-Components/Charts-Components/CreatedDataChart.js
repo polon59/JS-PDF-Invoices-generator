f7b8891f27545099a819860f4efa3f10901c60b5
polon59 <przemyslaw.polczak@gmail.com>
2019-03-20 04:20:45 +0100

    Add import for 'StatisticsDataParser' in 'Statistics' component

--- public/application/src/components/Statistics-Components/Charts-Components/CreatedDataChart.js
+++ public/application/src/components/Statistics-Components/Charts-Components/CreatedDataChart.js
@@ -1,5 +1,5 @@
 import React, { PureComponent } from 'react';
-import {AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip,} from 'recharts';
+import {AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer} from 'recharts';
 import { curveCardinal } from 'd3-shape';
 
 class CreatedDataChart extends PureComponent{
@@ -10,25 +10,25 @@ class CreatedDataChart extends PureComponent{
         this.state = {
           currentChartData : [
             {
-              name: 'Page A', uv: 4000, pv: 2400, amt: 2400,
+              name: 'Page A', uv: 4000,
             },
             {
-              name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
+              name: 'Page B', uv: 3000,
             },
             {
-              name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
+              name: 'Page C', uv: 2000,
             },
             {
-              name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
+              name: 'Page D', uv: 2780,
             },
             {
-              name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
+              name: 'Page E', uv: 1890,
             },
             {
-              name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
+              name: 'Page F', uv: 2390,
             },
             {
-              name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
+              name: 'Page G', uv: 3490,
             },
           ]
         }
@@ -37,7 +37,7 @@ class CreatedDataChart extends PureComponent{
 
     handleDataChange =() =>{
       this.setState({
-        data :[
+        currentChartData :[
           {
             name: 'Page A', uv: 3,
           },
@@ -65,23 +65,25 @@ class CreatedDataChart extends PureComponent{
 
       render() {
         return (
-          <div>
+           <div style={{ width: '100%', height: 300 }}>
             <button onClick={this.handleDataChange}>changedata</button>
-            <AreaChart
-              width={500}
-              height={400}
-              data={this.state.currentChartData}
-              margin={{
-                top: 10, right: 30, left: 0, bottom: 0,
-              }}
-            >
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="name" />
-              <YAxis />
-              <Tooltip />
-              <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" fillOpacity={0.3} />
-              {/* <Area type={this.cardinal} dataKey="uv" stroke="#82ca9d" fill="#82ca9d" fillOpacity={0.3} /> */}
-            </AreaChart>
+            <ResponsiveContainer>
+              <AreaChart
+                width={500}
+                height={400}
+                data={this.state.currentChartData}
+                margin={{
+                  top: 10, right: 30, left: 0, bottom: 0,
+                }}
+              >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" fillOpacity={0.3} />
+                {/* <Area type={this.cardinal} dataKey="uv" stroke="#82ca9d" fill="#82ca9d" fillOpacity={0.3} /> */}
+              </AreaChart>
+            </ResponsiveContainer>
           </div>
         );
       }
--- public/application/src/components/Statistics-Components/Statistics.js
+++ public/application/src/components/Statistics-Components/Statistics.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 import CreatedDataChart from './Charts-Components/CreatedDataChart';
+import StatisticsDataParser from './data-parser/DataParser';
 
 class Statistics extends Component{
     constructor(props){
         super();
+        this.dataParser = new StatisticsDataParser();
         this.DBAccess = props.DBAccess;
         this.state = {
             data : null
@@ -12,46 +14,13 @@ class Statistics extends Component{
 
     componentWillMount() {
         this.DBAccess.getStatisticsForYear(2019).then((result)=>{
-            let fullFormattedData = [];
-            // let servicesStats = result[0];
-            // let invoicesStats = result[1]['numberOfInvoices'];
-            console.log(result);
-
-            // console.log(servicesStats);
-            result.forEach((dataElement) => {
-                let currentStatsName = Object.keys(dataElement)[0];
-                let formattedDataElement =  {} ;
-                formattedDataElement[currentStatsName] = [];
-                for (let monthNo = 1; monthNo < 13; monthNo++) {
-                    let dataElements = dataElement[Object.keys(dataElement)[0]]
-                    let formattedRecord = {};
-                    formattedRecord['month'] = monthNo;
-                    formattedRecord[currentStatsName] = 0;
-                    dataElements.forEach(record => {
-                        if (record.month === monthNo) {
-                            console.log(record)
-                            formattedRecord[currentStatsName] = record[Object.keys(record)[1]];
-                        }
-                    });
-                    formattedDataElement[currentStatsName].push(formattedRecord);
-                    
-                }
-                fullFormattedData.push(formattedDataElement);
-            });
-            console.log(fullFormattedData);
-
-            // servicesStats.forEach(element => {
-            //     console.log(`month ${element.month} done services: ${element.doneServices}`);
-            // });
-
-            // invoicesStats.forEach(element => {
-            //     console.log(`month ${element.month} created Invoices: ${element.createdInvoices}`);
-            // });
-
-
+            let parsedData = this.dataParser.parseDataForStatistics(result);
+           
+            console.log(parsedData);
+            
 
             this.setState({
-                data: result,
+                data: parsedData,
             });
         })
     }
