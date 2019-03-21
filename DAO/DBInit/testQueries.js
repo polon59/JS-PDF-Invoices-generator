0de7c40dc739cb772be75a1bfb9b71ee4f886478
polon59 <przemyslaw.polczak@gmail.com>
2019-03-21 21:03:16 +0100

    Test select queries for 'StatisticsDAO.js'

--- DAO/DBInit/testQueries.js
+++ DAO/DBInit/testQueries.js
@@ -6,12 +6,8 @@ runQuery();
 // SELECT MONTH(date) as month, COUNT(*) as doneServices
 function runQuery() {
     const year = 2019;
-    const sql =`SELECT billTo as Company, ROUND(SUM(total),2) as Profit FROM services 
-        JOIN invoices ON invoices.id = services.invoiceID
-        WHERE YEAR(date) = ${year}
-        GROUP BY Company
-        ORDER BY Profit DESC
-        LIMIT 5;`;
+    const sql =`SET CHARACTER SET utf8;
+    `;
 
 
     connection.query(sql, function (err, result, fields) {
