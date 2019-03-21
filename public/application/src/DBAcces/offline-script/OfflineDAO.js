287bb957d87a71027fa7dd567c7cf315a6ca0234
polon59 <przemyslaw.polczak@gmail.com>
2019-03-21 23:34:42 +0100

    Implement methods for saving offline changes locally

--- public/application/src/DBAcces/offline-script/OfflineDAO.js
+++ public/application/src/DBAcces/offline-script/OfflineDAO.js
@@ -2,7 +2,7 @@ class OfflineDAO{
     
     constructor(DBAcces){
         this.DBAcces = DBAcces;
-        this.invoicesGeneratorOfflineData = initializeDataToSave();
+        this.invoicesGeneratorOfflineData = this.initializeDataToSave();
         // this.invoicesGeneratorOfflineData = {
         //     add:[],
         //     update:[],
@@ -15,6 +15,33 @@ class OfflineDAO{
             return {add:[], update:[], delete:[]}
         }
         return localStorage.getItem('invoicesGeneratorOfflineData');
+    }
+
+    addDataToSave = (data,method)=>{
+        if (method === 'add') {
+            this.addNewInvoice(data);
+        } 
+        else if (method === 'update') {
+            this.updateInvoice(data);
+        }
+        else{
+            this.deleteInvoice(data);
+        }
+    }
+
+    addNewInvoice = (invoiceToAdd) =>{
+        this.invoicesGeneratorOfflineData['add'].push(invoiceToAdd);
+        console.log(this.invoicesGeneratorOfflineData);
+    }
+
+    updateInvoice = (updatedInvoice) =>{
+        this.invoicesGeneratorOfflineData['update'].push(updatedInvoice);
+        console.log(this.invoicesGeneratorOfflineData);
+    }
+
+    deleteInvoice = (invoiceToDeleteId) =>{
+        this.invoicesGeneratorOfflineData['delete'].push(invoiceToDeleteId);
+        console.log(this.invoicesGeneratorOfflineData);
     }
 
 
