a4d8b2464df4d13549ced5746f8d5e40ee6df713
polon59 <przemyslaw.polczak@gmail.com>
2019-03-23 23:55:33 +0100

    Implement method 'setRouteGetAvailableStatisticsYears'

--- StatisticsController.js
+++ StatisticsController.js
@@ -9,10 +9,21 @@ class StatisticsController{
 
     setRoutes(){
         this.setRouteGetStatisticsForGivenYear();
+        this.setRouteGetAvailableStatisticsYears();
+    }
+
+    setRouteGetAvailableStatisticsYears(){
+        this.app.get('/myAccount/statistics/years', (req,res) =>{
+            this.statisticsDAO.getAvailableStatisticsYears()
+            .then( result=>{
+                res.send(JSON.stringify(result));
+            })
+            .catch(error => {res.send(`ERROR`)});
+        });
     }
 
     setRouteGetStatisticsForGivenYear(){
-        this.app.get('/myAccount/statistics/:year', (req,res) =>{
+        this.app.get('/myAccount/statistics/getFor/:year', (req,res) =>{
             const year = req.params.year;
             this.statisticsDAO.getAllStatistics(year)
             .then((result)=>{
