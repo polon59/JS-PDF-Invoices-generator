c3c1e552f7bc614de8529609b28bde28d7832175
polon59 <przemyslaw.polczak@gmail.com>
2019-03-24 17:07:04 +0100

    Add 'common' folder for componennts used in multiple places

---
+++ public/application/src/components/common/DialogSelect.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  formControl: {
+    margin: theme.spacing.unit,
+    minWidth: 120,
+  },
+});
+
+class DialogSelect extends React.Component {
+
+    constructor(props){
+        super();
+        this.state = {
+            open: false,
+            age: '',
+            options: props.options
+            };
+    }
+
+  handleChange = name => event => {
+    this.setState({ [name]: Number(event.target.value) });
+  };
+
+  handleClickOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
+  pepareOptionsSelection = () =>{
+      const {options} = this.state;
+      return options.map(option =>{
+      return (<MenuItem key={option} value={option}>{option}</MenuItem>);
+      })
+  }
+
+  render() {
+    const { classes,title,handleSubmit } = this.props;
+
+    return (
+      <div>
+        <Button onClick={this.handleClickOpen}>Open select dialog</Button>
+        <Dialog
+          disableBackdropClick
+          disableEscapeKeyDown
+          open={this.state.open}
+          onClose={this.handleClose}
+        >
+          <DialogTitle>Fill the form</DialogTitle>
+          <DialogContent>
+            <form className={classes.container}>
+              <FormControl className={classes.formControl}>
+                <InputLabel htmlFor="dialog-select">{title}</InputLabel>
+                <Select
+                  value={this.state.age}
+                  onChange={this.handleChange('age')}
+                  input={<Input id="dialog-select" />}
+                >
+                {this.pepareOptionsSelection()}
+
+                </Select>
+              </FormControl>
+            </form>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleClose} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={()=>{this.handleClose();handleSubmit(this.state.age)}} color="primary">
+              Ok
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    );
+  }
+}
+
+DialogSelect.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(DialogSelect);
