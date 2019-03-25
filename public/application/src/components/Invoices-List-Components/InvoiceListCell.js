d057c46a4a190555d00a128c1c0f725ce7519dd3
polon59 <przemyslaw.polczak@gmail.com>
2019-03-25 19:34:47 +0100

    Add import {Link} from 'react-router-dom' in 'InvoiceListCell' component

--- public/application/src/components/Invoices-List-Components/InvoiceListCell.js
+++ public/application/src/components/Invoices-List-Components/InvoiceListCell.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import {Link} from 'react-router-dom';
 
 const InvoiceListCell = (props) =>{
 
     let location = "online"
-    if (invoice.isOffline) {location = "offline"}
     const {changeCurrentInvoice,deleteInvoice,invoice,index} = props;
+    if (invoice.isOffline) {location = "offline"}
 
     return (
         <tr key={invoice.id}>
