351a8de74d79cda9d02c4046d4a0acfffc494e44
polon59 <przemyslaw.polczak@gmail.com>
2019-03-26 16:37:06 +0100

    Add 'StillLoading' component

---
+++ public/application/src/components/Statistics-Components/Wrong-Stats-Data-Components/StillLoading.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+const styles = theme => ({
+  progress: {
+    margin: theme.spacing.unit * 2,
+  },
+});
+
+function StillLoading(props) {
+  const { classes } = props;
+  return (
+    <div>
+      <CircularProgress className={classes.progress} />
+      <CircularProgress className={classes.progress} color="secondary" />
+    </div>
+  );
+}
+
+StillLoading.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(StillLoading);
