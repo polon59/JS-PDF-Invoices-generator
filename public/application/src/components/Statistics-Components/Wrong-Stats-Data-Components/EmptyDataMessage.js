dfcfa08d15b1c77a4065d0e790b4db62bb772d40
polon59 <przemyslaw.polczak@gmail.com>
2019-03-26 18:02:32 +0100

    Add new 'Paper' section with info message

--- public/application/src/components/Statistics-Components/Wrong-Stats-Data-Components/EmptyDataMessage.js
+++ public/application/src/components/Statistics-Components/Wrong-Stats-Data-Components/EmptyDataMessage.js
@@ -3,16 +3,22 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import NotInterested from '@material-ui/icons/NotInterested'
+import NotInterested from '@material-ui/icons/NotInterested';
+import Info from '@material-ui/icons/Info';
 
 const styles = theme => ({
   root: {
     flexGrow: 1,
   },
-  icon:{
-      height:'100%',
-      color: 'rgb(204, 204, 0)',
-      fontSize: 40
+  warningIcon:{
+    color: 'rgb(204, 204, 0)',
+    height:'100%',
+    fontSize: 40
+  },
+  infoIcon:{
+    color: 'rgb(0, 102, 255)',
+    height:'100%',
+    fontSize: 40
   },
   paper: {
     padding: theme.spacing.unit * 2,
@@ -38,12 +44,10 @@ function EmptyDataMessage(props) {
           <Paper className={classes.paper}>
             <Grid container spacing={24}>
                 <Grid item sm={2} xs={12}>
-                    <Paper className={classes.paperIcon}>
-                        <NotInterested className={classes.icon}/>
-                    </Paper>
+                        <NotInterested className={classes.warningIcon}/>
                 </Grid>
                 <Grid item sm={10} xs={12}>
-                    <h4>You have no active invoices. Statistics data cannot be displayed.</h4>
+                    <h3>You have no active invoices. Statistics data cannot be displayed.</h3>
                 </Grid>
               </Grid>
           </Paper>
@@ -51,7 +55,16 @@ function EmptyDataMessage(props) {
 
         <Grid item xs={12}>
           <Paper className={classes.paper}>
-                Add new invoice with at least one service, to display statistics for this year.
+            <Grid container spacing={24}>
+                <Grid item sm={2} xs={12}>
+                        <Info className={classes.infoIcon}/>
+                </Grid>
+                <Grid item sm={10} xs={12}>
+                    <h3>
+                        Add new invoice with at least one service, to display statistics for this year.
+                    </h3>
+                </Grid>
+              </Grid>
           </Paper>
         </Grid>
 
