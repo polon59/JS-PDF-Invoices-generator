a0177c9886b778c3e4011c1d9a8c17cd4735cf12
polon59 <przemyslaw.polczak@gmail.com>
2019-03-29 00:33:24 +0100

    Add basic structure of new 'EditInvoice' component DOM content

--- public/application/src/components/Edit-Invoice-Components/EditInvoice.js
+++ public/application/src/components/Edit-Invoice-Components/EditInvoice.js
@@ -2,6 +2,41 @@ import React, { Component } from 'react';
 import Services from './Services';
 import InvoiceSummary from './InvoiceSummary';
 import {withRouter} from 'react-router-dom';
+import Paper from '@material-ui/core/Paper';
+import TrendingUp from '@material-ui/icons/TrendingUp';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+
+
+const styles ={
+    root: {
+      flexGrow: 1,
+    },
+    container:{
+      marginTop:40,
+      height:'100%'
+    },
+    item:{
+      height:'100%'
+    },
+    paper: {
+      backgroundColor: 'white',
+      padding: 10,
+      color: 'black',
+    },
+    invoiceHeader:{
+        margin:0,
+        backgroundColor:'rgb(52, 109, 245)',
+    },
+    headerContainer:{
+        margin:0
+    },
+    invoiceLogo:{
+        color:'white',
+        fontSize:150,
+    },
+  };
+
 
 class EditInvoice extends Component{
 
@@ -53,49 +88,76 @@ class EditInvoice extends Component{
             )
         }
         return(
-            <div className="bordered">
+            <Paper style={styles.paper}>
                 <form onSubmit={this.handleSubmit}>
-                    <div className="invoice-header invoice-section">
-                        <input 
-                            type="text"
-                            id="title"
-                            placeholder="TITLE"
-                            value={title}
-                            onChange={this.handleChange}
-                            required
-                        />
-                        <input
-                            id="date"
-                            type="date"
-                            className="right" 
-                            value={date} 
-                            onChange={this.handleChange} 
-                            required
-                        />
-                    </div>
-                    <div className="invoice-company invoice-section">
-                        <textarea 
-                            id="billTo" 
-                            placeholder="BILL TO" 
-                            value={billTo} 
-                            onChange={(e)=>{
-                                this.handleChange(e); 
-                                this.resize(e.target);
-                            }} 
-                            required
-                        />
-                        <textarea 
-                            id="billFrom"
-                            placeholder="BILL FROM"
-                            className="right"
-                            value={billFrom}
-                            onChange={(e)=>{
-                                this.handleChange(e);
-                                this.resize(e.target)
-                            }} 
-                            required
-                        />
-                    </div>  
+                    <Paper style={styles.invoiceHeader}>
+                        <Grid style={styles.headerContainer} container spacing={24}>
+                            <Grid style={styles.invoiceLogo} item sm={4} xs={12}>
+                                <TrendingUp/>
+                            </Grid>
+
+                            <Grid item sm={8} xs={12}>
+                            <TextField
+                                id="billFrom"
+                                label="Bill from"
+                                placeholder="Who is this bill from?"
+                                multiline
+                                rowsMax="5"
+                                value={billFrom}
+                                onChange={(e)=>{this.handleChange(e);this.resize(e.target)}} 
+                                margin="normal"
+                                helperText="helper"
+                                variant="filled"
+                                required
+                            />
+                            </Grid>
+                        </Grid>
+                    </Paper>
+
+                    <Grid style={styles.container} container spacing={24}>
+                        <Grid item sm={6} xs={12}>
+                            <TextField
+                                id="billTo"
+                                label="Bill to"
+                                placeholder="Who is this bill to?"
+                                multiline
+                                rowsMax="5"
+                                value={billTo}
+                                onChange={(e)=>{this.handleChange(e);this.resize(e.target)}} 
+                                margin="normal"
+                                helperText="helper"
+                                variant="filled"
+                                required
+                            />
+                        </Grid>
+
+                        <Grid item sm={6} xs={12}>
+                            <TextField
+                                id="date"
+                                label="Date"
+                                type="date"
+                                value={date} 
+                                onChange={this.handleChange} 
+                                InputLabelProps={{
+                                shrink: true,
+                                }}
+                                required
+                            />
+                            <TextField
+                                id="title"
+                                label="Invoice title"
+                                placeholder="Select invoice title or ID"
+                                multiline
+                                rowsMax="2"
+                                value={title}
+                                onChange={this.handleChange}
+                                margin="normal"
+                                helperText="hello"
+                                variant="filled"
+                            />
+
+                        </Grid>
+                    </Grid>
                     <Services 
                         handleServiceDelete={this.handleServiceDelete} 
                         calculateSubTotal={calculateSubTotal} 
@@ -106,7 +168,14 @@ class EditInvoice extends Component{
                     <InvoiceSummary invoiceToEdit={invoiceToEdit} handleChange={this.handleChange}/>               
                     <input type="submit" value="Save invoice"/>
                 </form>
-            </div>
+            </Paper>
+
+
+
+
+
+
+
         )
     }
 }
