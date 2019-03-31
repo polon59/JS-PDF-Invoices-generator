2eb8b03d3dc1a3945f3ba1a1961dbf8e00fad11a
polon59 <przemyslaw.polczak@gmail.com>
2019-03-31 15:14:25 +0200

    Add newe styles fot 'VievInvoice' component

--- public/application/src/components/View-Invoice-Components/ViewInvoice.js
+++ public/application/src/components/View-Invoice-Components/ViewInvoice.js
@@ -3,6 +3,64 @@ import ViewServices from './ViewServices';
 import ViewInvoiceSummary from './ViewInvoiceSummary';
 import PDFGenerator from './PDF-generators/PDFGenerator';
 import {Link} from 'react-router-dom';
+import Paper from '@material-ui/core/Paper';
+import Fingerprint from '@material-ui/icons/Fingerprint';
+import Grid from '@material-ui/core/Grid';
+
+const styles ={
+    root: {
+      flexGrow: 1,
+    },
+    container:{
+      marginTop:40,
+      height:'100%'
+    },
+    item:{
+      height:'100%'
+    },
+    paper: {
+      display: 'inline-block',
+      boxSizing: 'border-box',
+      width: 850,
+      maxWidth: '100%',
+      minHeight:1000,
+      backgroundColor: 'white',
+      padding: 10,
+      color: 'black',
+    },
+    invoiceHeader:{
+        margin:0,
+        backgroundColor:'rgb(52, 109, 245)',
+    },
+    dateContainer:{
+        paddingLeft:'10%',
+        textAlign: 'left'
+    },
+    headerContainer:{
+        marginTop:5,
+        color:'white'
+    },
+    invoiceLogo:{
+        color:'white',
+        fontSize:70,
+    },
+    logoDesc:{
+        color:'white',
+        margin:0
+    },
+    billFrom:{
+        width:'80%'
+    },
+    billTo:{
+        width:'80%'
+    },
+    title:{
+        width:'80%'
+    },
+    date:{
+        width:'80%'
+    }
+  };
 
 class ViewInvoice extends Component{
 
@@ -22,30 +80,46 @@ class ViewInvoice extends Component{
         } else {
             const {id,title,date,billFrom,billTo,services} = invoiceToEdit;
             return(
-            <div className="bordered">
-                <h3>VIEW INVOICE</h3>
-                <div id="PDF-Content">
-                    <div className="invoice-header invoice-section">
-                        <h4>{title}</h4>
-                        <h4>{date}</h4>
-                    </div>
-                    <div className="invoice-company invoice-section">
-                        <h4>{billTo}</h4>
-                        <h4>{billFrom}</h4>
-                    </div>
+
+            <div className="editInvoice-wrapper">
+            <Paper id="PDF-Content" style={styles.paper}>
+                    <Paper style={styles.invoiceHeader}>
+                        <Grid style={styles.headerContainer} container spacing={24}>
+                            <Grid item sm={5} xs={12}>
+                                <Fingerprint style={styles.invoiceLogo}/>
+                                <h3 style={styles.logoDesc}>Your logo</h3>
+                            </Grid>
+                            <Grid  item sm={7} xs={12}>
+                                <h4>{billFrom}</h4>
+                            </Grid>
+                        </Grid>
+                    </Paper>
+                    <Grid style={styles.container} container spacing={24}>
+                        <Grid style={styles.dateContainer} item sm={5} xs={12}>
+                            <h3>{title}</h3>
+                            <br/>
+                            <h4>{date}</h4>
+                        </Grid>
+                        <Grid item sm={7} xs={12}>
+                            <h4>{billTo}</h4>
+                        </Grid>
+                    </Grid>
+
                     <div className="invoice-services">
-                        <ViewServices services={services}/>
-                    </div>
-                    <div className="invoice-summary">
-                        <ViewInvoiceSummary invoiceToEdit={invoiceToEdit}/>
-                    </div>
+                    <ViewServices services={services}/>
                 </div>
-                <div id="editor"></div>
+                <div className="invoice-summary">
+                    <ViewInvoiceSummary invoiceToEdit={invoiceToEdit}/>
+                </div>
                 <button id="cmd" onClick={this.generatePDFFromInvoice}>generate PDF</button>
                 <Link to={`/myInvoices/editInvoice/${id}`}>
                     <button onClick={()=>{changeCurrentInvoice(id)}}>EDIT</button>
                 </Link>
+            </Paper>
             </div>
+
+
+
             )
         }
     }
