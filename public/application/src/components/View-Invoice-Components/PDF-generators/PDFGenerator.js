c4e86468f8556b5a030fd93647bbf8028152704c
polon59 <przemyslaw.polczak@gmail.com>
2019-04-03 20:27:59 +0200

    Remove commented code lines fron 'PDFGenerator.js'

--- public/application/src/components/View-Invoice-Components/PDF-generators/PDFGenerator.js
+++ public/application/src/components/View-Invoice-Components/PDF-generators/PDFGenerator.js
@@ -12,26 +12,6 @@ class PDFGenerator{
             doc.addImage(img, 'JPEG',0,0);
             doc.save('document.pdf');
         });
-
-        // html2canvas(invoice, {
-        //     onrendered:function (canvas){
-        //         const img = canvas.toDataURL("image/png");
-        //         let doc = new JsPDF();
-        //         doc.addImage(img, 'JPEG',20,20);
-        //         doc.save('document.pdf');
-        //     }});
-
-        // let specialElementHandlers = {
-        //     '#editor': function (element, renderer) {
-        //         return true;
-        //     }
-        // };
-
-        // this.doc.fromHTML(invoice, 10, 10, {
-        //     'width': 170,
-        //     'elementHandlers': specialElementHandlers
-        // });
-        // this.doc.save('document.pdf');
     }
 }
 
