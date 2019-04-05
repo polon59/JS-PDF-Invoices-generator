a8ab7367c4340fa6fbcc9fd01fbe63c4200acc45
polon59 <przemyslaw.polczak@gmail.com>
2019-04-05 21:56:52 +0200

    Remove unused imports

--- public/application/src/components/common/MemoryLocation.js
+++ public/application/src/components/common/MemoryLocation.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import {Link} from 'react-router-dom';
 import CheckCircle from '@material-ui/icons/CheckCircle';
 import OfflineBolt from '@material-ui/icons/OfflineBolt';
 import Tooltip from '@material-ui/core/Tooltip';
