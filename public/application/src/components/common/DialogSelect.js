e92b4e6719dfcb7b809399e9e766377c8586586e
polon59 <przemyslaw.polczak@gmail.com>
2019-04-10 21:50:00 +0200

    Remove unused imported modules from 'DialogSelect' component

--- public/application/src/components/common/DialogSelect.js
+++ public/application/src/components/common/DialogSelect.js
@@ -5,7 +5,6 @@ import { withStyles } from '@material-ui/core/styles';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogTitle from '@material-ui/core/DialogTitle';
 import InputLabel from '@material-ui/core/InputLabel';
 import Input from '@material-ui/core/Input';
 import MenuItem from '@material-ui/core/MenuItem';
