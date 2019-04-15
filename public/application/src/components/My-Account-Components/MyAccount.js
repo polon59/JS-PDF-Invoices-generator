44152edddd24db96b24aa0e9338140a272d7dc39
polon59 <przemyslaw.polczak@gmail.com>
2019-04-15 19:21:46 +0200

    Import 'AccountOptions' component and add it to redurned content in 'render'

--- public/application/src/components/My-Account-Components/MyAccount.js
+++ public/application/src/components/My-Account-Components/MyAccount.js
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Avatar from '@material-ui/core/Avatar';
+import AccountOptions from './AccountOptions';
 
 
 const styles ={
@@ -51,25 +52,7 @@ class MyAccount extends Component{
             </Grid>
         </Paper>
 
-        <Paper style={styles.paper}>
-            <Grid container spacing={24}>
-                <Grid  item sm={4} xs={12}>
-                    <Link to='/myInvoices'>
-                        <div className="optionBox bordered"><h4>View current invoices</h4></div>
-                    </Link>
-                </Grid>
-                <Grid  item sm={4} xs={12}>
-                    <Link to='/addInvoice'>
-                        <div className="optionBox bordered"><h4>Create new invoice</h4></div>
-                    </Link>
-                </Grid>
-                <Grid  item sm={4} xs={12}>
-                    <Link to='/statistics'>
-                        <div className="optionBox bordered"><h4>View statistics</h4></div>
-                    </Link>
-                </Grid>
-            </Grid>
-        </Paper>
+        <AccountOptions/>
 
     </div>
 
