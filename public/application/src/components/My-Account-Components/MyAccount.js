b7323a38a73ba4cbe0abea22db56fbecc8ae34e0
polon59 <przemyslaw.polczak@gmail.com>
2019-04-15 18:15:43 +0200

    Add 'Paper' styles to 'MyAccount' page sections

--- public/application/src/components/My-Account-Components/MyAccount.js
+++ public/application/src/components/My-Account-Components/MyAccount.js
@@ -21,43 +21,45 @@ class MyAccount extends Component{
         return(
 
     <div>
+        <Paper style={styles.paper}>
             <Grid container alignContent='center' spacing={24}>
-                <Paper style={styles.paper}>
-                    <Grid item  sm={3} xs={12}>
-                        <p>logo</p>
-                    </Grid>
+                <Grid item  sm={3} xs={12}>
+                    <p>logo</p>
+                </Grid>
 
-                    <Grid item sm={9} xs={12}>
-                        <p>name of user</p>
-                    </Grid>
+                <Grid item sm={9} xs={12}>
+                    <p>name of user</p>
+                </Grid>
 
-                    <Grid item sm={12} xs={12}>
-                        <p>
-                            Welcome! this is some text added to look like it is nessesary, but honestly i added it to 
-                            fill this empty space
-                        </p>
-                    </Grid>
-                </Paper>
-
-                <Paper style={styles.paper}>
-                    <Grid  item sm={4} xs={12}>
-                        <Link to='/myInvoices'>
-                            <div className="optionBox bordered"><h4>View current invoices</h4></div>
-                        </Link>
-                    </Grid>
-                    <Grid  item sm={4} xs={12}>
-                        <Link to='/addInvoice'>
-                            <div className="optionBox bordered"><h4>Create new invoice</h4></div>
-                        </Link>
-                    </Grid>
-                    <Grid  item sm={4} xs={12}>
-                        <Link to='/statistics'>
-                            <div className="optionBox bordered"><h4>View statistics</h4></div>
-                        </Link>
-                    </Grid>
-                </Paper>
-
+                <Grid item sm={12} xs={12}>
+                    <p>
+                        Welcome! this is some text added to look like it is nessesary, but honestly i added it to 
+                        fill this empty space
+                    </p>
+                </Grid>
             </Grid>
+        </Paper>
+
+        <Paper style={styles.paper}>
+            <Grid container alignContent='center' spacing={24}>
+                <Grid  item sm={4} xs={12}>
+                    <Link to='/myInvoices'>
+                        <div className="optionBox bordered"><h4>View current invoices</h4></div>
+                    </Link>
+                </Grid>
+                <Grid  item sm={4} xs={12}>
+                    <Link to='/addInvoice'>
+                        <div className="optionBox bordered"><h4>Create new invoice</h4></div>
+                    </Link>
+                </Grid>
+                <Grid  item sm={4} xs={12}>
+                    <Link to='/statistics'>
+                        <div className="optionBox bordered"><h4>View statistics</h4></div>
+                    </Link>
+                </Grid>
+            </Grid>
+        </Paper>
+
     </div>
 
         );
