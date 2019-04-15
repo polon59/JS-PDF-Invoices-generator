71eb9accf3c68a29dad39305ba47eef2c896d27d
polon59 <przemyslaw.polczak@gmail.com>
2019-04-15 19:22:15 +0200

    Add JSX returned content to 'AccountOptions' component

--- public/application/src/components/My-Account-Components/AccountOptions.js
+++ public/application/src/components/My-Account-Components/AccountOptions.js
@@ -1,6 +1,27 @@
 import React from 'react';
 
 const AccountOptions = (props) =>{
+    return (
+        <Paper style={styles.paper}>
+            <Grid container spacing={24}>
+                <Grid  item sm={4} xs={12}>
+                    <Link to='/myInvoices'>
+                        <div className="optionBox bordered"><h4>View current invoices</h4></div>
+                    </Link>
+                </Grid>
+                <Grid  item sm={4} xs={12}>
+                    <Link to='/addInvoice'>
+                        <div className="optionBox bordered"><h4>Create new invoice</h4></div>
+                    </Link>
+                </Grid>
+                <Grid  item sm={4} xs={12}>
+                    <Link to='/statistics'>
+                        <div className="optionBox bordered"><h4>View statistics</h4></div>
+                    </Link>
+                </Grid>
+            </Grid>
+        </Paper>
+    )
 
 }
 
