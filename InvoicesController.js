a28218d8a81cd559a61e5b58dd3ccafa25e16ba4
polon59 <przemyslaw.polczak@gmail.com>
2019-04-23 23:10:14 +0200

    change indentation in 'InvoicesController.js'

--- InvoicesController.js
+++ InvoicesController.js
@@ -3,53 +3,53 @@ const InvoicesDAO = require('./DAO/InvoicesDAO');
 const DbCon = require('./DAO/dbConn');
 
 class InvoicesController{
-    
-        constructor(app,connection){
-            this.app = app;
-            this.invoicesDAO = new InvoicesDAO(connection);
-        }
 
-        setRoutes(){
-            this.setRouteAddingNewInvoice();
-            this.setRouteDisplayingAllInvoices();
-            this.setRoutePostEditingInvoice();
-            this.setRouteDeleteInvoice();
-        }
-
-        setRouteAddingNewInvoice(){
-            this.app.put('/myAccount/invoices', (req,res) =>{
-                this.invoicesDAO.addNewInvoice(req)
-                .then((assignedID)=>{
-                    res.send({"idValue":assignedID});
-                })
-                .catch(error => {console.log(error.message)});
-            });
-        }
-
-        setRouteDisplayingAllInvoices(){
-            this.app.get('/myAccount/invoices', (req, res) =>{
-                this.invoicesDAO.getAllInvoices()
-                .then((invoices)=>{
-                    res.send(invoices);
-                })
-                .catch(error => {console.log(error.message)});
-            });
-        }
-
-        setRouteDeleteInvoice(){
-            this.app.delete('/myAccount/invoices/:invoiceId' , (req,res) =>{
-                const id = req.params.invoiceId;
-                this.invoicesDAO.deleteInvoice(id);
-                res.send(`INVOICE DELETED`);
-            });
-        }
-
-        setRoutePostEditingInvoice(){
-            this.app.post('/myAccount/invoices/edit/:invoiceId' , (req,res) =>{
-                this.invoicesDAO.updateInvoice(req);
-                res.send(`INVOICE SAVED`);
-            })
-        }
+    constructor(app,connection){
+        this.app = app;
+        this.invoicesDAO = new InvoicesDAO(connection);
     }
-    
-    module.exports = InvoicesController;
+
+    setRoutes(){
+        this.setRouteAddingNewInvoice();
+        this.setRouteDisplayingAllInvoices();
+        this.setRoutePostEditingInvoice();
+        this.setRouteDeleteInvoice();
+    }
+
+    setRouteAddingNewInvoice(){
+        this.app.put('/myAccount/invoices', (req,res) =>{
+            this.invoicesDAO.addNewInvoice(req)
+            .then((assignedID)=>{
+                res.send({"idValue":assignedID});
+            })
+            .catch(error => {console.log(error.message)});
+        });
+    }
+
+    setRouteDisplayingAllInvoices(){
+        this.app.get('/myAccount/invoices', (req, res) =>{
+            this.invoicesDAO.getAllInvoices()
+            .then((invoices)=>{
+                res.send(invoices);
+            })
+            .catch(error => {console.log(error.message)});
+        });
+    }
+
+    setRouteDeleteInvoice(){
+        this.app.delete('/myAccount/invoices/:invoiceId' , (req,res) =>{
+            const id = req.params.invoiceId;
+            this.invoicesDAO.deleteInvoice(id);
+            res.send(`INVOICE DELETED`);
+        });
+    }
+
+    setRoutePostEditingInvoice(){
+        this.app.post('/myAccount/invoices/edit/:invoiceId' , (req,res) =>{
+            this.invoicesDAO.updateInvoice(req);
+            res.send(`INVOICE SAVED`);
+        })
+    }
+}
+
+module.exports = InvoicesController;
