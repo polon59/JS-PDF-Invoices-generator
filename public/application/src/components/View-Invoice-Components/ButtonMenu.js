ecca51807c7acbb451135b7133fb395f0d0efdc9
polon59 <przemyslaw.polczak@gmail.com>
2019-05-08 15:55:15 +0200

    Implement 'render' method in 'ButtonMenu' component

--- public/application/src/components/View-Invoice-Components/ButtonMenu.js
+++ public/application/src/components/View-Invoice-Components/ButtonMenu.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import Button from '@material-ui/core/Button';
 
 class ButtonMenu extends React.Component{
 
@@ -7,8 +9,21 @@ class ButtonMenu extends React.Component{
     }
 
     render(){
+        const {generatePDFFromInvoice, changeCurrentInvoice, id} = this.props;
         return(
-            
+            <div>
+                <Button variant="outlined" onClick={generatePDFFromInvoice}>
+                    Download invoice in PDF
+                </Button>
+                <Button 
+                    component={Link} 
+                    to={`/myInvoices/editInvoice/${id}`} 
+                    variant="outlined" 
+                >
+                    Edit this Invoice
+                </Button>
+                    
+            </div>
         )
     }
 }
