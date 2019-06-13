784cdf72c8fb93eee542d4fc1a41fcff641d465f
polon59 <przemyslaw.polczak@gmail.com>
2019-06-13 17:42:22 +0200

    Invoke controllers initialization in DB connection check callback

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const MessageDisplay = require('./common/MessageDisplay');
 const InvoicesController = require('./InvoicesController.js');
 const AccountController = require('./AccountController.js');
 const StatisticsController = require('./StatisticsController');
@@ -8,19 +9,20 @@ const app = express();
 const port = process.env.PORT || 8000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
+const messageLog = new MessageDisplay();
+const dataBaseConn = new DbCon(messageLog);
 
-const dataBaseConn = new DbCon();
-const connection = dataBaseConn.createDBConnection();
+dataBaseConn.createDBConnection().then((connection)=>{
+   if (connection == null) {
+       process.exit();
+   }
+   var invoicesController = new InvoicesController(app,connection);
+   var accountController = new AccountController(app);
+   var statisticsController = new StatisticsController(app,connection);
+   invoicesController.setRoutes();
+   accountController.setRoutes();
+   statisticsController.setRoutes();
+   
+   app.listen(port, () => {console.log(`*** server listening on port ${port} ***`)});
+});
 
-if (connection == null) {
-    process.exit();
-}
-
-var invoicesController = new InvoicesController(app,connection);
-var accountController = new AccountController(app);
-var statisticsController = new StatisticsController(app,connection);
-invoicesController.setRoutes();
-accountController.setRoutes();
-statisticsController.setRoutes();
-
-app.listen(port, () => {console.log(`*** server listening on port ${port} ***`)});
