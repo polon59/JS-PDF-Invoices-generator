c587fa9db9b3b8f84b16e1d13ea5c8b318c2717a
polon59 <przemyslaw.polczak@gmail.com>
2019-06-13 16:32:16 +0200

    Define color symbols in 'MessageDisplay' class constructor

--- common/MessageDisplay.js
+++ common/MessageDisplay.js
@@ -1,7 +1,21 @@
 
 class MessageDisplay{
     constructor(){
-
+        this.reset = "\x1b[0m";
+        this.underscore = "\x1b[4m";
+        this.colors = {
+            red : "\x1b[31m",
+            green : "\x1b[32m",
+            yellow : "\x1b[33m",
+            blue : "\x1b[34m"
+        }
+        this.backgrounds = {
+            red : "\x1b[41m",
+            green : "\x1b[42m",
+            yellow : "\x1b[43m",
+            blue : "\x1b[44m",
+            white : "\x1b[47m"
+        }
     }
 }
 
