3a38b9d9224e2275990d779d63290e4882ae8c27
polon59 <przemyslaw.polczak@gmail.com>
2019-06-13 15:59:20 +0200

    Implement method 'isConnectionSuccessful' in 'DBConn.js'

--- DAO/dbConn.js
+++ DAO/dbConn.js
@@ -2,18 +2,39 @@ class DbConn{
   
     constructor(){
       this.mysql = require('mysql');
-      this.connection = this.createDBConnection();
+      this.host = "localhostr";
+      this.user = "root";
+      this.password = "mysQLpassword";
+      this.database = "mydb";
     }
   
     createDBConnection(){
-      let connection = this.mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "mysQLpassword",
-        database: "mydb"
+      const {host,user,password,database} = this;
+      const connection = this.mysql.createConnection({
+        host: host,
+        user: user,
+        password: password,
+        database: database
       });
-      console.log("-- DBConn: successfully connected to database");
-      return connection;
+      if (this.isConnectionSuccessful(connection)){
+        return connection;
+      }
+      return null;
+    }
+
+    
+    isConnectionSuccessful(connection){
+      try {
+        connection.createConnection();
+      } catch (error) {
+        console.log(`[DB Connection]: ERROR connecting to database. 
+        Authentication failed or database does not exist.
+        Check host, user, password and DB data in ./DAO/DBConn.js file`);
+        return false;
+      }
+
+      console.log("[DB Connection]: successfuly connected to database");
+      return true;
     }
   
   }
