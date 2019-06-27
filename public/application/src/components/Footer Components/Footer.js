a9dd77d87532b998bc3d0911c25b79997b3832e8
polon59 <przemyslaw.polczak@gmail.com>
2019-06-27 19:04:38 +0200

    Add new footer content

--- public/application/src/components/Footer Components/Footer.js
+++ public/application/src/components/Footer Components/Footer.js
@@ -1,33 +1,24 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
 
 const Footer = (props) =>{
     return(
         <footer>
-            <div className="container-fluid text-center">
-                <div className="row">
-                <div className="col-sm-4">
-                    <a className="navOpt" href="aboutUs.html"><p>O firmie</p></a>
-                    <a className="navOpt" href="projects.html"><p>Realizacje</p></a>
-                    <a className="navOpt" href="contact.html"><p>Kontakt</p></a>
-                </div>
-                <div className="col-sm-4">
-                    <p>Mirosław Pawełczak</p>
-                    <p>Podole 63</p>
-                    <p>39-320 Przecław</p>
-                </div>
-                <div className="col-sm-4">
-                    <p>Zakład Ślusarski</p>
-                    <p>Tel: +48 696 399 748</p>
-                    <p>e-mail: p@gmail.com</p>
-                </div>
-                </div> 
-                <div className="row footer-text-bottom">   
-                <p>
-                    Designed and built by @polon59 for Ślusarstwo Pawełczak 
-                    2019 <span className="glyphicon glyphicon-globe"></span>
-                </p>
-                </div>
-            </div>
+            <Grid container spacing={24}>
+                <Grid item sm={4} xs={12}>
+                    <h4>area 1</h4>
+                </Grid>
+                <Grid  item sm={4} xs={12}>
+                    <h4>area 2</h4>
+                </Grid>
+                <Grid  item sm={4} xs={12}>
+                    <h4>area 3</h4>
+                </Grid>
+            </Grid>
+            <p>
+                Designed and built by @polon59 for Ślusarstwo Pawełczak 
+                2019 <span className="glyphicon glyphicon-globe"></span>
+            </p>
         </footer>
     )
 }
