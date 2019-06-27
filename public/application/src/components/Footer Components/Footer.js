22ddb5096f0568a84f035bfa4b6250409ea55956
polon59 <przemyslaw.polczak@gmail.com>
2019-06-27 20:00:22 +0200

    Add footer content icons

--- public/application/src/components/Footer Components/Footer.js
+++ public/application/src/components/Footer Components/Footer.js
@@ -1,24 +1,45 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
+import Help from '@material-ui/icons/Help';
+import AddComment from '@material-ui/icons/AddComment';
+
+const styles = {
+    root:{
+        height: '10%',
+        marginTop: 20,
+        backgroundColor: 'rgb(52, 109, 245)',
+        color: 'rgb(255, 255, 255)',
+        width: '100%',
+        padding: 10,
+        textAlign : 'center'
+    },
+    colMiddle: {
+        fontSize: 'small'
+    },
+    colSide :{
+        fontSize : 'medium'
+    },
+    icon:{
+        fontSize : 'medium',
+        marginRight: 5
+    }
+    
+}
 
 const Footer = (props) =>{
     return(
-        <footer>
+        <footer style={styles.root}>
             <Grid container spacing={24}>
-                <Grid item sm={4} xs={12}>
-                    <h4>area 1</h4>
+                <Grid style={styles.colSide} item sm={4} xs={12}>
+                    <p><AddComment style={styles.icon}/>Contact administrator</p>
                 </Grid>
-                <Grid  item sm={4} xs={12}>
-                    <h4>area 2</h4>
+                <Grid style={styles.colMiddle} item sm={4} xs={12}>
+                   <p>Designed and built by @polon59  InvoiceGenerator 2019</p>
                 </Grid>
-                <Grid  item sm={4} xs={12}>
-                    <h4>area 3</h4>
+                <Grid style={styles.colSide} item sm={4} xs={12}>
+                    <p><Help style={styles.icon}/>Help</p>
                 </Grid>
             </Grid>
-            <p>
-                Designed and built by @polon59 for Ślusarstwo Pawełczak 
-                2019 <span className="glyphicon glyphicon-globe"></span>
-            </p>
         </footer>
     )
 }
